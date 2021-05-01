a730b1a4babea666297880f20fb60db1f34ad3bd
junrey <junreydotarot@gmail.com>
2021-05-01 19:23:04 +0300

    install and setup laratrust

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,10 +73,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->attachRole('admin'); 
+
+        return $user;
     }
 }
